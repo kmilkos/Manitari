85dad0af04993788e21b06ad4df1206ce86944c9
Paragogi <Paragogi@DESKTOP-5PJV1KK>
2018-05-17 10:16:16 +0300

    Metric Table First Edition

---
+++ Manitari/Manitari.Module/BusinessObjects/Metric.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Xpo
+Imports DevExpress.ExpressApp
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.DC
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.Base
+Imports System.Collections.Generic
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.Validation
+
+'<ImageName("BO_Contact")> _
+'<DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")> _
+'<DefaultListViewOptions(MasterDetailMode.ListViewOnly, False, NewItemRowPosition.None)> _
+'<Persistent("DatabaseTableName")> _
+<DefaultClassOptions()> _
+Public Class Metric ' Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    Inherits BaseObject ' Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+    Public Sub New(ByVal session As Session)
+        MyBase.New(session)
+    End Sub
+    Public Overrides Sub AfterConstruction()
+        MyBase.AfterConstruction()
+        ' Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+    End Sub
+
+    Private _metricName As String
+    <Size(100)>
+    Property MetricName As String
+        Get
+            Return _metricName
+        End Get
+        Set(ByVal Value As String)
+            SetPropertyValue(Nameof(MetricName), _metricName, Value)
+        End Set
+    End Property
+    Private _description As String
+    <Size(200)>
+    Property Description As String
+        Get
+            Return _description
+        End Get
+        Set(ByVal Value As String)
+            SetPropertyValue(Nameof(Description), _description, Value)
+        End Set
+    End Property
+    
+End Class
